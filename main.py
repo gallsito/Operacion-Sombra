6aa65b00962df824b485aa9b8a0e4bfcaa26b03d
TomasRossi06 <torossi@uade.edu.ar>
2026-03-26 16:58:34 -0300

    Creacion de la rama feature_level_1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import random
+abcedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+palabra = "Agente"
+
+def generardesplazamiento():
+    return random.randint(1, 10)
+
+def cifrado_cesar(texto, desplazamiento):
+    resultado = ""
+    for letra in texto:
+        if letra in abcedario:
+            indice = (abcedario.index(letra) + desplazamiento) % len(abcedario)
+            resultado += abcedario[indice]
+        else:
+            resultado += letra
+    return resultado
+
+
+
 
